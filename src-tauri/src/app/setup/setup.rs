984fe53990cff6a622a2997d26e2e0da54551993
Darth Buddha <Darth.Buddha@hotmail.com>
2025-01-19 17:44:46 -0500

    0.0.63

--- src-tauri/src/app/setup/setup.rs
+++ src-tauri/src/app/setup/setup.rs
@@ -10,6 +10,7 @@ use std::sync::Arc;
 // Tauri
 use tauri::{ AppHandle, Emitter, Manager };
 // Dependencies
+// use log::{ error, info };
 use tokio::time::{ sleep, Duration };
 use tokio::sync::Mutex;
 // Crates
@@ -24,9 +25,9 @@ pub async fn setup(app: AppHandle) -> Result<(), ()> {
   {
     let mut state = setup_state.lock().await;
     // Perform setup tasks while holding the lock
-    println!("Performing really heavy backend setup task...");
+    log::info!("Performing really heavy backend setup task...");
     sleep(Duration::from_secs(3)).await;
-    println!("Backend setup task completed!");
+    log::info!("Backend setup task completed!");
     state.backend_task = true;
   }
   // Emit an event to the frontend indicating that the backend setup is complete
